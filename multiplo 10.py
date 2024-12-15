a7b93439d8fc026e75bff8de957f0c8b9a60a00e
YasbelAgr <yasbelnac06@gmail.com>
2024-12-14 21:59:53 -0600

    first commit

---
+++ multiplo 10.py
@@ -0,0 +1,7 @@
+numero = 0
+numero = int(input("I ngresa un numero por favor"))
+if numero % 10 == 0:
+    print("El numero ",numero," es multiplo de 10")
+else:
+    print("El numero ",numero," no es multiplo de 10")
+      
